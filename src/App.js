32ae90558619c4ebf9c5b0b52e5150cc9b3fc65c
Matthew Henderson <mattcharlesh@gmail.com>
2020-11-19 20:24:22 -0500

    basic modal added

--- src/App.js
+++ src/App.js
@@ -2,43 +2,26 @@ import './App.css';
 import { useState, useEffect } from 'react';
 import UserCards from './UserCards';
 
-// import Modal from './Modal'
+import Modal from './Modal';
 
-// const BUTTON_WRAPPER_STYLES = {
-//   position: 'relative',
-//   zIndex: 1
-// }
+const BUTTON_WRAPPER_STYLES = {
+  position: 'relative',
+  zIndex: 1,
+};
 
-// const OTHER_CONTENT_STYLES = {
-//   position: 'relative',
-//   zIndex: 2,
-//   backgroundColor: 'red',
-//   padding: '10px'
-// }
-
-// function () {
-//   const [isOpen, setIsOpen] = useState(false)
-//   return (
-//     <>
-//       <div style={BUTTON_WRAPPER_STYLES} onClick={() => console.log('clicked')}>
-//         <button onClick={() => setIsOpen(true)}>Open Modal</button>
-
-//         <Modal open={isOpen} onClose={() => setIsOpen(false)}>
-//           Fancy Modal
-//         </Modal>
-//       </div>
-
-//       <div style={OTHER_CONTENT_STYLES}>Other Content</div>
-//     </>
-//   )
-// }
-
+const OTHER_CONTENT_STYLES = {
+  position: 'relative',
+  zIndex: 2,
+  backgroundColor: 'red',
+  padding: '10px',
+};
 
 function App() {
   const [users, setUsers] = useState([]);
   const [loading, setLoading] = useState(false);
   const [search, setSearch] = useState('');
   const [filteredUsers, setFilteredUsers] = useState([]);
+  const [isOpen, setIsOpen] = useState(false);
 
   const useFetch = async () => {
     //https://javascript.info/async-await#error-handling
@@ -56,24 +39,36 @@ function App() {
 
   useEffect(() => {
     setFilteredUsers(
-      users.filter( user => {
+      users.filter((user) => {
         return (
           user.name.first.toLowerCase().includes(search.toLowerCase()) ||
           user.name.last.toLowerCase().includes(search.toLowerCase())
-        )
-      
+        );
       })
-    )
-  },[search, users])
+    );
+  }, [search, users]);
 
   return (
     <div className='App'>
+      <div style={BUTTON_WRAPPER_STYLES} onClick={() => console.log('clicked')}>
+        <button onClick={() => setIsOpen(true)}>Open Modal</button>
+
+        <Modal open={isOpen} onClose={() => setIsOpen(false)}>
+          Nice Modal
+        </Modal>
+      </div>
+
+      <div style={OTHER_CONTENT_STYLES}>Other Content</div>
+
       <div className='header'>
         <h1>Random Users</h1>
 
         {/* https://www.youtube.com/watch?v=Q8JyF3wpsHc&t=173s */}
-        <input type='text' placeholder='Search' 
-         onChange={e => setSearch(e.target.value)} />
+        <input
+          type='text'
+          placeholder='Search'
+          onChange={(e) => setSearch(e.target.value)}
+        />
 
         <button className='fetch-button' onClick={useFetch}>
           Fetch Data
@@ -81,7 +76,7 @@ function App() {
       </div>
 
       <div className='users'>
-        {loading && console.log('loading') }
+        {loading && console.log('loading')}
         {users && <UserCards users={filteredUsers} />}
       </div>
     </div>
